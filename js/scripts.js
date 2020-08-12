88a40179f7bfdc9520703160d658a35f136a6baa
Nehagan08 <nehagandhmal123@gmail.com>
2020-08-12 17:07:45 +0530

    Added function to delete an item and Mark or unmark the tark as completed ( & Related Styling )

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,9 @@ window.onload = function() {
 
     const done_svg = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="nofill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
 
+    const list = document.getElementById('to-do');
+    const completed = document.getElementById('completed');
+
     document.getElementById("add-item").addEventListener('click', () => {
         //console.log("Button clicked..");
         let value = document.getElementById("item").value;
@@ -18,8 +21,6 @@ window.onload = function() {
 
     //adding new item to list
     function addItemToDo(text) {
-        let list = document.getElementById('to-do');
-
         let item = document.createElement('li');
         item.innerText = text;
 
@@ -40,5 +41,31 @@ window.onload = function() {
         item.appendChild(buttons);
 
         list.insertBefore(item, list.childNodes[0]);
+
+        delete_btn.addEventListener('click', deleteItemFromList);
+        done_btn.addEventListener('click', completeTaskFromList);
+    }
+
+    function deleteItemFromList() {
+        let item = this.parentNode.parentNode;
+        let parent = item.parentNode;
+
+        parent.removeChild(item);
+
+        //list.removeChild(this.parentNode.parentNode);
+        //'this' is button to be clicked, 1st parentNode : buttons, 2nd parentNode : li
+    }
+
+    function completeTaskFromList() {
+        let item = this.parentNode.parentNode;
+        let parent = item.parentNode;
+
+        //Checking that item should be added to completed list or to-do list
+        let target = (parent.id === 'to-do')?completed:list;
+
+        console.log(target);
+
+        parent.removeChild(item);
+        target.insertBefore(item, target.childNodes[0]);
     }
 }
